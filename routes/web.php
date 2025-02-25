9842cf7b99587c27334d9661683165987e37dc67
unknown <fawwaz.alifio.farsa@gmail.com>
2025-02-25 15:46:17 +0700

    added view & controller for level

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,10 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', HomeController::class);
-
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
-});
-
-Route::get('/user/{id}/name/{name}', UserController::class);
-
-Route::get('/transaksi', TransactionController::class);
+Route::get('/level', [LevelController::class, 'index']);
