3b1bb2690666b44b57779349ea2a0e946eaa8915
unknown <fawwaz.alifio.farsa@gmail.com>
2025-02-25 22:12:08 +0700

    added user model, view, and route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function __invoke($id, $nama)
+    public function index()
     {
-        return view('user.index')
-        ->with('nama', $nama)
-        ->with('id', $id);
+        // Tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+
+        UserModel::where('username', 'customer-1')->update($data); // Update data user
+
+        // Coba akses model UserModel
+        $user = UserModel::all(); // Ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
     }
 }
