b4fc2e3ede157ad9e30672533bdfa0f9c003615d
unknown <fawwaz.alifio.farsa@gmail.com>
2025-03-26 16:19:23 +0700

    jobsheet 7 tugas 3

--- routes/web.php
+++ routes/web.php
@@ -97,20 +97,34 @@ Route::middleware(['auth'])->group(function () {
         Route::delete('/{id}', [SupplierController::class, 'destroy']);
     });
 
-    Route::group(['prefix' => 'barang'], function () {
-        Route::get('/', [BarangController::class, 'index']);
-        Route::post('/list', [BarangController::class, 'list']);
-        Route::get('/create', [BarangController::class, 'create']);
-        Route::post('/', [BarangController::class, 'store']);
-        Route::get('/create_ajax', [BarangController::class, 'create_ajax']);
-        Route::post('/ajax', [BarangController::class, 'store_ajax']);
-        Route::get('/{id}', [BarangController::class, 'show']);
-        Route::get('/{id}/edit', [BarangController::class, 'edit']);
-        Route::put('/{id}', [BarangController::class, 'update']);
-        Route::get('/{id}/edit_ajax', [BarangController::class, 'edit_ajax']);
-        Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']);
-        Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
-        Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
-        Route::delete('/{id}', [BarangController::class, 'destroy']);
+    Route::middleware(['authorize:ADM,MNG'])->group(function(){
+        Route::get('/barang', [BarangController::class, 'index']);
+        Route::post('/barang/list', [BarangController::class, 'list']);
+
+        Route::get('/barang/create_ajax', [BarangController::class, 'create_ajax']); // ajax form create
+        Route::post('/barang_ajax', [BarangController::class, 'store_ajax']); // ajax store
+
+        Route::get('/barang/{id}/edit_ajax', [BarangController::class, 'edit_ajax']); // ajax form edit
+        Route::put('/barang/{id}/update_ajax', [BarangController::class, 'update_ajax']); // ajax update
+
+        Route::get('/barang/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']); // ajax form confirm
+        Route::delete('/barang/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); // ajax delete
     });
+
+    // Route::group(['prefix' => 'barang'], function () {
+    //     Route::get('/', [BarangController::class, 'index']);
+    //     Route::post('/list', [BarangController::class, 'list']);
+    //     Route::get('/create', [BarangController::class, 'create']);
+    //     Route::post('/', [BarangController::class, 'store']);
+    //     Route::get('/create_ajax', [BarangController::class, 'create_ajax']);
+    //     Route::post('/ajax', [BarangController::class, 'store_ajax']);
+    //     Route::get('/{id}', [BarangController::class, 'show']);
+    //     Route::get('/{id}/edit', [BarangController::class, 'edit']);
+    //     Route::put('/{id}', [BarangController::class, 'update']);
+    //     Route::get('/{id}/edit_ajax', [BarangController::class, 'edit_ajax']);
+    //     Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']);
+    //     Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
+    //     Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
+    //     Route::delete('/{id}', [BarangController::class, 'destroy']);
+    // });
 });
